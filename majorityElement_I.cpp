08106686935983ed81e9ec5462608303d26f5490
hack-abdul <mominabdulrehmaan@gmail.com>
2023-07-01 03:02:46 +0530

    DAY3 PROBLEM3

--- majorityElement_I.cpp
+++ majorityElement_I.cpp
@@ -2,20 +2,15 @@
 
 int findMajorityElement(int arr[], int n) {
 	// Write your code here.
-	if(n == 1) return arr[0];
-	int ans = arr[0], count = 1, finalCount = 0;
-	for(int i=1; i<n; i++){
-		if(arr[i] != ans){
-			count--;
-			if(count == 0){
-				ans = arr[i];
-				count++;
-			}
-		} else count++;
-	}
-	for(int i=0; i<n; i++){
-		if(arr[i] == ans) finalCount++;
-	}
-	if(finalCount > n/2) return ans;
-	return -1;
+	int ele = arr[0], count = 0;
+        for(int i=0; i<n; i++){
+            if(count==0) ele = arr[i];
+            count += (ele == arr[i])?1:-1;
+        }
+		count = 0;
+		for(int i=0; i<n; i++){
+			if(arr[i] == ele) count++;
+		}
+		if(count > n/2) return ele;
+        return -1;
 }
